c0fba218486fe9ec39ba71039c49ff856671100e
Vic Wu <madcocomo@gmail.com>
2020-12-30 22:32:24 +0800

    remove unused files

--- commit.sh
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "commit: $1"
-git push --set-upstream origin HEAD
--- signin.sh
+++
@@ -1,7 +0,0 @@
-if [ -z "$1" ]
-then
-    echo "usage ./signin.sh <You ID>"
-    exit 1
-fi
-
-git checkout -b $1
